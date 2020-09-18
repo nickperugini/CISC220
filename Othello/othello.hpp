dcc36ff65451ec705f23b1b45c743b1abb1871ec
Suryanuj Gupta <54590991+suryanuj@users.noreply.github.com>
2020-09-18 15:31:49 -0400

    Update othello.hpp

--- Othello/othello.hpp
+++ Othello/othello.hpp
@@ -1,10 +1,9 @@
 /*
  * othello.hpp
  *
- *
- *  Created on: Sep 13, 2020
- *      Author: Nick Perugini
- *      Partner: Suryanuj Gupta
+ *  Created on: Sep 11, 2020
+ *  Author: Nicholas Perugini
+ *  Partner: Suryanuj Gupta
  */
 
 #ifndef OTHELLO_HPP_
@@ -12,33 +11,29 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
-#include "player.cpp"
+#include "player.hpp"
 using namespace std;
 
 //static int boardSize = 8;
 
 class Othello {
-public:
-	char matrix[9][9];
-	Player player1;
-	Player player2;
-	int numPlayers;
+	public:
+		char matrix[9][9];
+		Player player1;
+		Player player2;
+		int numPlayers;
 
 		Othello();
 		Othello(string, char);
 		Othello(string, char, string, char);
+
 		void playGame();
 		void makemat();
 		void printmat();
 		void placepiece(string, char);
 		int countandflippieces(int, int, string, char, bool);
 		void ckwin();
-
-	void compplacepiece(Player p){
-
-	}
-
-
+		void compplacepiece(Player p);
 };
 
 #endif /* OTHELLO_HPP_ */
